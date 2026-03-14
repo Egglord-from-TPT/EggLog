34db46b942bf6a810744c437193ace782ec6b0df
Egglord-from-TPT <karlamongus6767@gmail.com>
2026-03-14 12:59:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="egg",
-    version="1.0.1",
+    version="1.0.2",
     packages=find_packages(),
     description="A python library that logs all output.",
     author="",
